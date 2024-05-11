838d59a49e4bbe685ad01822d25bfff26376c5f7
shawntsai0312 <shawntsai0312@gmail.com>
2024-05-11 12:21:23 +0800

    merge complete

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,70 @@
+'use client';
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import './page.css'
+
+function Page() {
+    const router = useRouter();
+
+    return (
+        <div className="App">
+            <div className={'mainContainer'}>
+                <div className={'titleContainer'}>
+                    <div>Sign Up</div>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        placeholder="Enter your username here"
+                        className={'inputBox'}
+                    />
+                    <label className="errorLabel">{ }</label>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        placeholder="Enter your profile here"
+                        className={'inputBox'}
+                    />
+                    <label className="errorLabel">{ }</label>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        placeholder="Enter your password here"
+                        className={'inputBox'}
+                    />
+                    <label className="errorLabel">{ }</label>
+                </div>
+                <br />
+                <div className="form-group">
+                    <select className="form-control">
+                        <option value="select">Select your character</option>
+                        <option value="First">Shop Owner</option>
+                        <option value="Second">Artist</option>
+                    </select>
+                </div>
+                <br />
+                <div className="form-group">
+                    <select className="form-control">
+                        <option value="select">Select your style</option>
+                        <option >Carpentry</option>
+                        <option >Interior design</option>
+                        <option >Architecture design</option>
+                        <option >Sculpture</option>
+                        <option >Paiting</option>
+                        <option >Music</option>
+                        <option >Comics</option>
+                        <option >Fragrance</option>
+                    </select>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input className={'buttonContainer'} type="button" value={'sign up'} onClick={()=>router.push('/home')} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page
